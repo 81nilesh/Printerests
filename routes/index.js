2286133d1532a98d9ed1d422e4b86db661fa0050
ankitchoudhary8120@gmail.com <ankitchoudhary8120@gmail.com>
2023-12-11 22:13:58 +0530

    post complete

--- routes/index.js
+++ routes/index.js
@@ -22,17 +22,27 @@ router.get('/feed', function (req, res, next) {
   res.render('feed');
 });
 
-router.post('/upload', upload.single("file"), function (req, res, next) {
+router.post('/upload', isLoggedIn, upload.single("file"), async function (req, res, next) {
   if (!req.file) {
     return res.status(404).send("no files were given");
   }
-  res.send("file uploaded succesfully");
+  const user = await userModel.findOne({ username: req.session.passport.user });
+  const post = await postModel.create({
+    image: req.file.filename,
+    imageText: req.body.filecaption,
+    user: user._id
+  });
+  user.posts.push(post._id);
+  await user.save();
+  res.redirect("/profile");
 })
 
 router.get('/profile', isLoggedIn, async function (req, res, next) {
   const user = await userModel.findOne({
     username: req.session.passport.user
   })
+    .populate("posts")
+  console.log(user);
   res.render("profile", { user });
 });
 
